00c9dd88822363dc5c5a8b33ddc08fa1d462690e
Denis Mironov <denis.mironov@profitero.com>
2024-04-02 23:48:53 +0300

    update curl_test

--- curl_test.sh
+++ curl_test.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Выполнение запроса с помощью curl
-response=$(curl -s -X POST http://localhost:4567/route -H 'Content-Type: application/json' -d '{"lat": -8.670458199999999, "lng": 115.2126293, "opts": {"size": 2, "local": true, "radius": 100, "type": "car", "rounds": 5}}')
+# URL для запроса
+url="http://localhost:4567/route"
+# Данные JSON для запроса
+data='{"lat": -8.670458199999999, "lng": 115.2126293, "opts": {"size": 2, "local": true, "radius": 100, "type": "car", "rounds": 5}}'
+
+# Вывод запроса curl в терминал
+echo "Sending request:"
+echo "curl -X POST $url -H 'Content-Type: application/json' -d '$data'"
+echo ""
+
+# Выполнение запроса с помощью curl и сохранение ответа
+response=$(curl -X POST $url -H 'Content-Type: application/json' -d "$data" -v)
 
 # Проверка наличия ключа "result" в JSON ответе
 if echo "$response" | jq -e '.result' > /dev/null; then
@@ -16,9 +26,14 @@ else
     # Если ключ "result" отсутствует, вывод полного ответа и кода ответа
     echo "Response: $response"
     response_code=$(echo "$response" | jq -r '.code')
-    echo "BAD RESPOND"
+    echo "BAD RESPONSE"
 
     echo "Response code: $response_code"
+
+    # Вывод HTTP-заголовков ответа
+    echo "Response headers:"
+    curl -s -X POST $url -H 'Content-Type: application/json' -d "$data" -D - -o /dev/null
+
     # Неудачное завершение с кодом 1
     exit 1
 fi
